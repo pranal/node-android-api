ade18da30548fb35686bd5703a925351fc9d334f
Pranal <pranaldesai@gmail.com>
2018-03-14 00:12:51 +0530

    Added signup,login,logout

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require('./config/config');
+
 const express=require('express');
 const bodyParser=require('body-parser');
+const jwt=require('jsonwebtoken');
+const {ObjectID} = require('mongodb');
 const _=require('lodash');
+var {authenticate}=require('./middleware/authenticate');
+
+
 const {User}=require('./model/User');
 const{mongoose}=require('./db/mongoose');
 
@@ -11,22 +17,43 @@ const port = process.env.PORT||3000;
 
 app.use(bodyParser.json());
 
+
 app.post('/signup', (req, res) => {
     
 var body=_.pick(req.body,['email','password']);
 console.log(body);
 var user= new User(body);
 
-user.save().then(()=>{
-    console.log('User signed up');
-    res.status(200).send();
-}).catch((e)=>{
+user.save().then(() => {
+    
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
     res.status(400).send(e);
+  })
 });
 
-
-
-  });
+app.post('/login',(req,res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+  
+    User.findByCredentials(body.email,body.password).then((user)=>{
+      
+    return user.generateAuthToken().then((token)=>{
+     res.header('x-auth',token).send(user);
+     });
+    }).catch((e)=>{
+     res.status(400).send();
+    });
+  });  
+  
+  app.delete('/logout/token',authenticate,(req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+      res.status(200).send();
+    },()=>{
+     res.status(400).send();
+    });
+    });
   
 
 
--- config/config.js
+++ config/config.js
@@ -1,2 +1,4 @@
 process.env.PORT=3000;
 process.env.MONGODB_URI='mongodb://localhost:27017/WallApp';
+
+process.env.JWT_SECRET='123abc';
