7bbd0f6413a4c2efcbf15d0c69e57d95ff7aa434
Pranal <pranaldesai@gmail.com>
2018-03-14 00:48:14 +0530

    make it better

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,7 +2,7 @@ const {User}=require('./../model/User');
 
 var authenticate= (req,res,next)=>{
     var token =req.header('x-auth');
-  
+  console.log('tets');
     User.findByToken(token).then((user)=>{
      if(!user){
       return Promise.reject();
