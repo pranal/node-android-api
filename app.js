ab7c5255f00467b1ed4c32008a9ac5e23689ec87
SharmadDesai <sharmad.desai77@gmail.com>
2018-03-17 16:19:59 +0530

    Added file upload

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const jwt=require('jsonwebtoken');
 const nodemailer=require('nodemailer');
+const Grid = require("gridfs-stream");
+const busboyBodyParser = require('busboy-body-parser');
+
+
+
 const {ObjectID} = require('mongodb');
 const _=require('lodash');
 var {authenticate}=require('./middleware/authenticate');
@@ -15,15 +20,32 @@ const {User}=require('./model/User');
 const{Wallpaper}=require('./model/Wallpaper');
 const{Creator}=require('./model/Creator');
 const{mongoose}=require('./db/mongoose');
+const mongoose2 = require("mongoose");
 const{Favorite}=require('./model/Favorite');
 
 var creatorname=null;
 
+let conn = mongoose2.connection;
+Grid.mongo = mongoose2.mongo;
+
+// Grid.mongo=mongoose.mongo;
+let gfs;
+
+
+
+
+
 var app=express();
 const port = process.env.PORT||3000;
 
-
+app.use(busboyBodyParser({ limit: '10mb' }));  
 app.use(bodyParser.json());
+
+
+conn.once("open", () => {
+  gfs = Grid(conn.db);
+  
+
 
 
 app.post('/wallpapers',(req, res) => {
@@ -104,6 +126,7 @@ app.post('/send', authenticatec, (req, res) => {
  const output=`
  <p>You have a new creator request</p>
  <h3> Creator Details</h3>
+ <a href="http://wallpapers.openode.io/img/${filename}">Your Photo</a>
  <ul>
      <li>Name:${req.body.name}</li>
      <li>Email:${req.creator.email}</li>
@@ -226,8 +249,80 @@ app.post('/login',(req,res)=>{
      res.status(400).send();
     });
     });
+
+  
   
 
+     app.get('/file',(req,res)=>{
+        res.send('Hello');
+
+        })
+
+    app.post('/img', (req, res) => {
+      console.log(req);
+          let part = req.files.file;
+          let writeStream = gfs.createWriteStream({
+              filename: 'img_' + part.name,
+              mode: 'w',
+              content_type: part.mimetype
+          });
+  
+          writeStream.on('close', (file) => {
+            // checking for file
+            if(!file) {
+              res.status(400).send('No file received');
+            }
+              return res.status(200).send({
+                  message: 'Success',
+                  file: file
+              });
+          });
+          // using callbacks is important !
+          // writeStream should end the operation once all data is written to the DB 
+          writeStream.write(part.data, () => {
+            writeStream.end();
+          });  
+      });
+
+      app.get('/img/:imgname', (req, res) => {
+        let imgname = req.params.imgname;
+          gfs.files.find({
+              filename: imgname
+          }).toArray((err, files) => {
+  
+              if (files.length === 0) {
+                  return res.status(404).send({
+                      message: 'File not found'
+                  });
+              }
+              let data = [];
+              let readstream = gfs.createReadStream({
+                  filename: files[0].filename
+              });
+  
+              readstream.on('data', (chunk) => {
+                  data.push(chunk);
+              });
+  
+              readstream.on('end', () => {
+                  data = Buffer.concat(data);
+                  let img = 'data:image/png;base64,' + Buffer(data).toString('base64');
+                  res.end(img);
+              });
+  
+              readstream.on('error', (err) => {
+                // if theres an error, respond with a status of 500
+                // responds should be sent, otherwise the users will be kept waiting
+                // until Connection Time out
+                  res.status(500).send(err);
+                  console.log('An error occurred!', err);
+              });
+          });
+      });
+
+
+    });
+  
 
 
 
